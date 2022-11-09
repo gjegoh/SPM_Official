6c2f0cc0c1f1e98b7bcd9c834693d311c7923365
gjegoh <jiaen.goh.2020@scis.smu.edu.sg>
2022-11-09 17:58:52 +0800

    added unittests and docker compose command to run unittests

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -5,6 +5,10 @@ echo "###   Applying database migrations   ###"
 python manage.py makemigrations
 python manage.py migrate
 
+# Running django tests
+echo "###  Running django tests   ###"
+python manage.py test LJPS || exit 1
+
 # Start server
 echo "###   Starting server   ###"
 python manage.py runserver 0.0.0.0:8000
